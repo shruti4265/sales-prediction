ccdc828003e1b6465157c7146c63713b85e1b49d
Smriti Singh <smriti2815@gmail.com>
2025-04-23 08:23:46 +0530

    Fix indentation in app.py

--- app.py
+++ app.py
@@ -25,4 +25,7 @@ def predict():
     return render_template('index.html', prediction=round(prediction[0], 2))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(debug=True, host="0.0.0.0", port=port)
+
