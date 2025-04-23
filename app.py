7a7a75f30c94a86e20f54826ec9f3f9a6f56a348
Smriti Singh <smriti2815@gmail.com>
2025-04-23 07:39:45 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, render_template
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+import numpy as np
+
+app = Flask(__name__)
+
+# Load the data
+data = pd.read_csv('sales_data.csv')
+X = data[['Marketing Spend']]
+y = data['Sales']
+
+# Train model
+model = LinearRegression()
+model.fit(X, y)
+
+@app.route('/')
+def home():
+    return render_template('index.html', prediction=None)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    marketing_spend = float(request.form['marketing_spend'])
+    prediction = model.predict(np.array([[marketing_spend]]))
+    return render_template('index.html', prediction=round(prediction[0], 2))
+
+if __name__ == '__main__':
+    app.run(debug=True)
